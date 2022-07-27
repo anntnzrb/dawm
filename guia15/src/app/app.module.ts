907dad5734344d6978564fb70edb4240bafba3b0
anntnzrb <anntnzrb@protonmail.com>
2022-07-27 15:35:40 -0500

    guia15: base

---
+++ guia15/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';  
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import { SplashComponent } from './splash/splash.component';
+import { PrincipalComponent } from './principal/principal.component';
+import { AlbumComponent } from './album/album.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SplashComponent,
+    PrincipalComponent,
+    AlbumComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
