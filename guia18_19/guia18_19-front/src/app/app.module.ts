b4efd45f83485a976c1507af3814ec2cee116746
anntnzrb <anntnzrb@protonmail.com>
2022-08-13 22:04:02 -0500

    guia18/19: fin

--- guia18_19/guia18_19-front/src/app/app.module.ts
+++ guia18_19/guia18_19-front/src/app/app.module.ts
@@ -7,14 +7,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
 
 import { ListaComponent } from './lista/lista.component';
+import { ProductoComponent } from './producto/producto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListaComponent
+    ListaComponent,
+    ProductoComponent
   ],
   imports: [
     BrowserModule,
@@ -22,6 +26,8 @@ import { ListaComponent } from './lista/lista.component';
     BrowserAnimationsModule,
     MatTableModule,
     HttpClientModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [
     { provide: LocationStrategy, useClass: HashLocationStrategy }
